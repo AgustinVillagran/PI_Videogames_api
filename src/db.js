4bdb3185a97d8782d716d83e23e3105602a3518a
Marcelo Agustín Villagrán <magustin.villagran@gmail.com>
2022-04-17 00:24:46 -0300

    gitignore and fix env

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize, DataTypes, UUIDV4 } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 let sequelize = process.env.NODE_ENV === "production"
 ? new Sequelize(process.env.DATABASE_URL, {
@@ -18,7 +18,7 @@ let sequelize = process.env.NODE_ENV === "production"
   },
   ssl: true,
 })
-: new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}:${DB_PORT}@${DB_HOST}/${DB_NAME}`, {
+: new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
